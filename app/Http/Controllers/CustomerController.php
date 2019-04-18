c8fbf9af568e601b61013bf601aaa4bdce38c9bf
Nicholas Nuing <nicholas.nuing@gmail.com>
2019-04-18 17:50:00 +0800

    DEBUG request object.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -113,7 +113,7 @@ class CustomerController extends Controller
         // dd($request->request);
         $customer_car = Customercar::where('plate_no','like',$request->plate_no)->first();
         // dd($customer_car);
-        // dd(count($customer_car));
+        dd(count($customer_car));
         // $has_car = (count($customer_car) > 0) ? true : false;
         // dd($has_car);
 
