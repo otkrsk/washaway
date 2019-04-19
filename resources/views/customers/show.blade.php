341b64f20d72ad8d8d16736fdcf2ffbef92e8b9f
Nicholas Nuing <nicholas.nuing@gmail.com>
2019-04-19 20:51:31 +0800

    Display Member's Car Information on Member's Search Result page.

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
 
-<h3>{{ $customer->name }}</h3>
-<h4>Plate No: {{ $customer->plate_no }}</h4>
-<h4>Model: {{ $customer_car->brand()->first()->name }} {{ $customer_car->model()->first()->name }}</h4>
-<h4>Member Status: {{ ($customer->is_member) ? "Member" : "Walk-In" }}</h4>
-<h4 style="visibility:hidden;">Remaining Unclaimed Services (TBD)</h3>
+  <h3>{{ $customer->name }}</h3>
+  <h4>Plate No: {{ $customer->plate_no }}</h4>
+  <h4>Model: {{ $customer_car->brand()->first()->name }} {{ $customer_car->model()->first()->name }}</h4>
+  <h4>Member Status: {{ ($customer->is_member) ? "Member" : "Walk-In" }}</h4>
+  <h4 style="visibility:hidden;">Remaining Unclaimed Services (TBD)</h3>
 
-<a class='waves-effect waves-light btn'>Cancel</a>
-<a href='{{ route("customers.addservice", ["id" => $customer->id]) }}' class='waves-effect waves-light btn'>Confirm</a>
+  <a class='waves-effect waves-light btn'>Cancel</a>
+  <a href='{{ route("customers.addservice", ["id" => $customer->id]) }}' class='waves-effect waves-light btn'>Confirm</a>
+
 @endsection
--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -1,13 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<h3>Member</h3>
-<ul>
-  <li>Name: {{ $member->name }}</li>  
-  <li>Contact No: {{ $member->contact_no }}</li>  
-  <li>Member Status: {{ ($member->membership->is_expired) ? "Expired" : "Member" }}</li>  
-  <li>Expiry Date: {{ date('d-m-Y', strtotime($member->membership->membership_expires) ) }}</li>  
-  <li>Branch: {{ $member->branches->first()->name }}</li>  
-</ul>
+  <h3>Member</h3>
+  <ul>
+    <li>Name: {{ $member->name }}</li>  
+    <li>Contact No: {{ $member->contact_no }}</li>  
+    <li>Member Status: {{ ($member->membership->is_expired) ? "Expired" : "Member" }}</li>  
+    <li>Expiry Date: {{ date('d-m-Y', strtotime($member->membership->membership_expires) ) }}</li>  
+    <li>Branch: {{ $member->branches->first()->name }}</li>  
+  </ul>
+
+  <div class="row">
+    <h4>Main Car #1</h4>
+    <table>
+      <tbody>
+        <tr>
+          <td>Plate No:</td><td>{{ $member->cars->first()->plate_no }}</td>
+        </tr>
+        <tr>
+          <td>Make & Model:</td><td>{{ $member->cars->first()->brand()->first()->name }} {{ $member->cars->first()->model()->first()->name }}</td>
+        </tr>
+        <tr>
+          <td>Colour:</td><td>{{ $member->cars->first()->color()->first()->name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
 
 @endsection
