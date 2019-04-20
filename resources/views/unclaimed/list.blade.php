ceac939b0fd8d572bf14c68ec28f73fbc67d9ee0
Nicholas Nuing <nicholas.nuing@gmail.com>
2019-04-20 12:16:26 +0800

    Removed dummy text

--- resources/views/unclaimed/list.blade.php
+++ resources/views/unclaimed/list.blade.php
@@ -4,7 +4,6 @@
 
 <div class="row">
   <h3>Select Remaining Unclaimed</h3>
-  <h4>!! REMEMBER TO CONTINUE FROM HERE AFTER YOU'VE FINISHED EATING YOUR BREAKFAST AND WATCHING MODERN FAMILY !!</h4>
 </div>
 
 <div class="row">
